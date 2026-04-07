fefc79e826a1f3d54381907e22bb3ba177f3ad2f
YASHDHADOD <yash_dhadod@epam.com>
2026-04-07 22:05:18 +0530

    feat - dockerize flow

--- services/AuthService/src/lib/publish.py
+++ services/AuthService/src/lib/publish.py
@@ -1,10 +1,15 @@
 # user_service/events/publisher.py
 
+import os
 from redis.asyncio import Redis 
 import json
  
 
-redis_client = Redis(host="localhost", port=6381, db=0)
+redis_client = Redis(
+    host=os.getenv("REDIS_STREAM_HOST", "localhost"),
+    port=int(os.getenv("REDIS_STREAM_PORT", "6381")),
+    db=0,
+)
 
 async def publish_user_updated(user_id: str, username: str, avatar: str):
     payload = {
